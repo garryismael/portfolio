ca4b35ab4887e3e0151030806b8df752ed60f2c9
Garry Ismaël <tahinjanaharygarry@gmail.com>
2023-12-03 21:09:48 +0300

    setup projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  Component,
+  afterNextRender
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import AOS from 'aos';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  constructor() {
+    afterNextRender(() => {
+      AOS.init();
+    });
+  }
+
   title = 'portfolio';
 }
